ea0510bb3851b6e423e94dd058b3fb9a3ae87530
sibaprasad <sibap865@gmail.com>
2023-06-04 12:29:10 +0530

    data ingestion completed

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-from cnnClassfier import logger
+from cnnClassifier.components.dataingestion import DataIngestion
+from cnnClassifier.pipeline.stage01data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier import logger
 
-logger.info("welcome to my custom log")
+STAGE_NAME ="Data Ingestion stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from cnnClassfier import logger
+from cnnClassifier import logger
 import json
 import joblib
 from ensure import ensure_annotations
